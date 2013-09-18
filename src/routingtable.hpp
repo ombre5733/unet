8aba3a8961bf11f53ee871bfd4eeeda84397dde3
Manuel Freiberger <manuel.freiberger@gmx.at>
2013-09-18 02:08:55 +0200

    A bit more documentation

--- src/routingtable.hpp
+++ src/routingtable.hpp
@@ -9,11 +9,21 @@ namespace uNet
 class RoutingTableEntry
 {
 public:
+    NetworkAddress m_address;
+    Neighbor* m_neighbor;
+    //! If set, this entry is static.
+    bool m_static;
 };
 
+//! The routing table.
+//! The RoutingTable resolves a destination address to a neighbor address. In
+//! other words, for any in the network destination it returns the address to
+//! which the message must be routed next.
 class RoutingTable
 {
 public:
+    //void addStaticRoute(HostAddress destination);
+
     //! Resolves an address.
     //! Looks up the \p destination address in the routing table and returns
     //! the host address of the next neighbor, which is the next target for
@@ -21,6 +31,8 @@ public:
     HostAddress resolve(HostAddress destination) const;
 
 private:
+    //! The table entries.
+    RoutingTableEntry m_tableEntries[10];
 };
 
 } // namespace uNet
