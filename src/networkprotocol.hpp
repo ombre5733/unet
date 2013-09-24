055c6684df6e854a1265a080d9ce0e8645558805
Manuel Freiberger <manuel.freiberger@gmx.at>
2013-09-24 20:33:45 +0200

    Added a default constructor

--- src/networkprotocol.hpp
+++ src/networkprotocol.hpp
@@ -6,30 +6,40 @@
 namespace uNet
 {
 
-// The network protocol carries enough information to route data from one
-// interface in the network to another. It has the following header
-//
-// \code
-// 0                   1                   2                   3
-// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-// |Version|HopCnt |  Next header  |            Length             |
-// +-------+-------+---------------+---------------+---------------+
-// |        Source address         |      Destination address      |
-// +---------------+---------------+---------------+---------------+
-// \endcode
-//
-// The \p Version is the protocol version. The \p HopCnt field is decremented
-// whenever the message is routed. If the \p HopCnt is zero, the message is
-// not routed any longer but is dropped by the kernel.
-// The <tt>Next header</tt> encodes the type of the header in the payload.
-// The \p Length is the total length of the message including this header. The
-// <tt>Source address</tt> and <tt>Destination address</tt> fields contain
-// the source and destination addresses of the sending and receiving
-// interfaces.
+/*!
+Network Protocol
+
+The network protocol carries enough information to route data from one
+interface in the network to another. It has the following header:
+
+\code
+0                   1                   2                   3
+0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|Version|HopCnt |  Next header  |            Length             |
++-------+-------+---------------+---------------+---------------+
+|        Source address         |      Destination address      |
++---------------+---------------+---------------+---------------+
+\endcode
+
+The \p Version is the protocol version. The \p HopCnt field is decremented
+whenever the message is routed. If the \p HopCnt is zero, the message is
+not routed any longer but is dropped by the kernel.
+The <tt>Next header</tt> encodes the type of the header in the payload.
+The \p Length is the total length of the message including this header. The
+<tt>Source address</tt> and <tt>Destination address</tt> fields contain
+the source and destination addresses of the sending and receiving
+interfaces.
+*/
 
 struct NetworkProtocolHeader
 {
+    NetworkProtocolHeader()
+        : version(1),
+          hopCount(maxHopCount)
+    {
+    }
+
     std::uint8_t version : 4;
     std::uint8_t hopCount : 4;
     std::uint8_t nextHeader;
@@ -37,6 +47,7 @@ struct NetworkProtocolHeader
     std::uint16_t sourceAddress;
     std::uint16_t destinationAddress;
 
+    //! The maximum possible value for the hop count.
     static const std::uint8_t maxHopCount = 15;
 };
 
