7c1e9feba53f2addcb5f55b6acda0d3807c757b8
Manuel Freiberger <manuel.freiberger@gmx.at>
2013-10-07 19:44:54 +0200

    Simplified test cases

--- src/test/networkaddress/tst_networkaddress.cpp
+++ src/test/networkaddress/tst_networkaddress.cpp
@@ -1,44 +1,49 @@
 #include "../../networkaddress.hpp"
 
+#include <boost/type_traits/alignment_of.hpp>
+
 #include "gtest/gtest.h"
 
 TEST(HostAddress, MemoryLayout)
 {
     // We require the host address to be a standard-layout class such that
-    // we can copy it. TODO: clarify this with is_trivial
-    ASSERT_EQ(true, std::is_standard_layout<HostAddress>::value);
+    // we can copy it.
+    //! \todo clarify this with is_trivial
+    ASSERT_TRUE(std::is_standard_layout<uNet::HostAddress>::value);
 
-    ASSERT_EQ(2, sizeof(HostAddress));
+    ASSERT_EQ(2, sizeof(uNet::HostAddress));
+    ASSERT_TRUE(boost::alignment_of<uNet::HostAddress>::value <= 2);
 }
 
 TEST(HostAddress, Initialization)
 {
-    HostAddress addr1;
+    uNet::HostAddress addr1;
     ASSERT_EQ(0, addr1.address());
 
-    HostAddress addr2(0x0123);
+    uNet::HostAddress addr2(0x0123);
     ASSERT_EQ(0x0123, addr2.address());
 }
 
 TEST(HostAddress, ConversionOperator)
 {
-    HostAddress addr(0x1234);
+    uNet::HostAddress addr(0x1234);
     uint16_t u = addr;
     ASSERT_EQ(0x1234, u);
 }
 
 TEST(HostAddress, SubnetCheck)
 {
-    HostAddress addr(0x1234);
-    ASSERT_EQ(true, addr.isInSubnet(0x1200, 0xFF00));
-    ASSERT_EQ(true, addr.isInSubnet(0x12FF, 0xFF00));
-    ASSERT_EQ(false, addr.isInSubnet(0x1300, 0xFF00));
-    ASSERT_EQ(false, addr.isInSubnet(0x13FF, 0xFF00));
+    uNet::HostAddress addr(0x1234);
+    ASSERT_TRUE(addr.isInSubnet(0x1200, 0xFF00));
+    ASSERT_TRUE(addr.isInSubnet(0x12FF, 0xFF00));
+    ASSERT_FALSE(addr.isInSubnet(0x1300, 0xFF00));
+    ASSERT_FALSE(addr.isInSubnet(0x13FF, 0xFF00));
 }
 
 TEST(NetworkAddress, Initialization)
 {
-    NetworkAddress addr;
+    uNet::NetworkAddress addr;
     ASSERT_EQ(0, addr.hostAddress().address());
     ASSERT_EQ(0, addr.netmask());
 }
+
