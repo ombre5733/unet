5f1e54b81ce94a9fe26e5c18cfd4f97420c0b1f8
Manuel Freiberger <manuel.freiberger@gmx.at>
2013-10-08 21:52:46 +0200

    Added method to query if the link-layer address is unspecified

--- src/linklayeraddress.hpp
+++ src/linklayeraddress.hpp
@@ -8,12 +8,23 @@ namespace uNet
 
 //! A link-layer address.
 //! A LinkLayerAddress represents the address of an interface on the physical
-//! link. The size and contents of such an address is specific to the link.
+//! link. The size and contents of such an address is specific to the link's
+//! interface implementation.
+//! The LinkLayerAddress has enough storage to hold any link address. The
+//! only requirement is that a zero value represents an unspecified address.
 struct LinkLayerAddress
 {
     LinkLayerAddress()
         : address(0)
     {
+    }
+
+    //! Checks if this is an unspecified address.
+    //! Returns \p true, if this link-layer address is an unspecified address,
+    //! i.e. if it equals 0.
+    bool unspecified() const
+    {
+        return address == 0;
     }
 
     std::uint32_t address;
